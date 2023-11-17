b50990477e5302bfa08eb653c426d85eb328b8c0
john-raymon <john.raymon@ourbranch.com>
2023-11-16 19:28:34 -0500

    add /* and comment explaining it's purpose

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //this is the page user is redirected to after accepting data use on spotify's website
 //it does not have to be /account, it can be whatever page you want it to be
@@ -38,44 +38,46 @@ app.get("/callback", async (req, res) => {
   const access_token = spotifyRes.data.access_token;
   const refresh_token = spotifyRes.data.refresh_token;
   const maxAge = spotifyRes.data.expires_in;
-  const expiration = new Date(Number(new Date()) + (maxAge * 1000));
+  const expiration = new Date(Number(new Date()) + maxAge * 1000);
 
-  res.cookie('token', access_token, { expires: expiration, httpOnly: false });
-  res.cookie('refresh', refresh_token);
+  res.cookie("token", access_token, { expires: expiration, httpOnly: false });
+  res.cookie("refresh", refresh_token);
   res.redirect("/");
-})
+});
 
-app.get('/refresh', async (req, res) => {
+app.get("/refresh", async (req, res) => {
   const spotifyRes = await axios.post(
     "https://accounts.spotify.com/api/token",
     queryString.stringify({
       refresh_token: refreshToken,
-      grant_type: 'refresh_token',
+      grant_type: "refresh_token",
     }),
     {
       headers: {
-        Authorization: "Basic " + process.env.BASE64_AUTHORIZATION
+        Authorization: "Basic " + process.env.BASE64_AUTHORIZATION,
       },
     }
   );
   const access_token = spotifyRes.data.access_token;
   const maxAge = spotifyRes.data.expires_in;
-  const expiration = new Date(Number(new Date()) + (maxAge * 1000));
+  const expiration = new Date(Number(new Date()) + maxAge * 1000);
 
-  res.cookie('token', access_token, { expires: expiration, httpOnly: false });
-  res.send({ access_token, type: 'refresh' });
-})
+  res.cookie("token", access_token, { expires: expiration, httpOnly: false });
+  res.send({ access_token, type: "refresh" });
+});
 
-app.get('/api/log-out', (req, res) => {
-  res.clearCookie('token');
-  res.clearCookie('refresh');
+app.get("/api/log-out", (req, res) => {
+  res.clearCookie("token");
+  res.clearCookie("refresh");
   res.redirect("/");
-})
+});
 
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-})
+// adding /* will make it so that any route that is not defined will be redirected to the index.html file
+// so that react router can handle directing the user to the correct client side route
+// an alternative to /* is to pass an array of paths to app.get such as app.get(["/login", "/callback"], (req, res) => {...})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("App is listening on port 3000");
