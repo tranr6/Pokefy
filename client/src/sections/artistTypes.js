7b9e84d08977b20027be427120663f7af511866e
Johnny Ngo <johnnyhngo@yahoo.com>
2023-11-17 08:59:51 -0500

    artistype update

--- client/src/sections/artistTypes.js
+++ client/src/sections/artistTypes.js
@@ -1,29 +1,51 @@
-import React from "react";
-import Home from "./Home.js";
-//import { timeRangeFilters } from "../constants/filter.js";
+import React from 'react';
+import Home from './Home.js';
+import SelectOptions from "../components/Options.js";
+import { timeRangeFilters } from "../constants/filter.js";
+import { getTopArtists } from "../helper/spotify.js";
 
 export default class ArtistTypes extends React.Component {
-  render() {
-    const { token } = this.props;
-    let content;
-    // If we don't have a token, return to home
-    if (token.token === "") {
-      return <Home />;
+    constructor() {
+        super();
+        this.state = {
+            timeRange: '',
+        }
+        this.handleClick = this.handleClick.bind(this);
     }
-    // Proceed to ArtistTypes
-    else {
-      content = (
-        <div>
-          <h1>Select your team!</h1>
-          <div>
-            <button>Last Month</button>
-            <button>Last 6 Months</button>
-            <button>All Time</button>
-          </div>
-        </div>
-      );
+    handleClick(value) {
+        console.log(value)
+        this.setState({ timeRange: value }, () => {
+        });
+        // once clicked we will take the value (timeframe) and make API call
     }
-
-    return <div>{content}</div>;
-  }
+    render() {
+        const { token } = this.props;
+        let content;
+        // If we don't have a token, return to home
+        if (token.token === '') { return <Home /> }
+        // Proceed to ArtistTypes
+        else {
+            content =
+                <div>
+                    <h1>Select your team!</h1>
+                    {Object.keys(timeRangeFilters).map(key => {
+                        console.log(key);
+                        return <SelectOptions
+                            key={key}
+                            value={key}
+                            clickHandler={this.handleClick}
+                        >
+                            {timeRangeFilters[key]}
+                        </SelectOptions>
+                    })
+                    }
+                </div>
+        }
+        return (
+            <div>
+                {content}
+            </div>
+        )
+    }
 }
+   
